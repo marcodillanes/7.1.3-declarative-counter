49cea527e95280eb4007dce585486420252d5d9b
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-03-05 15:15:44 -0800

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
-function App() {
+function App(){
+  let [ counter, changeCounter ] = useState(1)
+  window.changeCounter = changeCounter
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>{counter}</h1>
+    <h4>{counter}</h4>
     </div>
-  );
+  )
 }
 
 export default App;
+
+//can also do this in one line like---> import React, { useState } from 'react'
+
+// step 3 calls for all of this to be deleted to begin fresh
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
